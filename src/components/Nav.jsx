8f0bfd690d80f8817f543097ffedd81d77ee14cd
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2025-01-13 19:42:23 +0100

    change nav to dark theme

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
-import { useDispatch } from "react-redux";
-import { openContactModal } from "../features/modals/modalSlice"; 
+import { useDispatch, useSelector } from "react-redux";
+import { openContactModal } from "../features/modals/modalSlice";
 import iconMessage from "../assets/icon_message.png";
 import "../styles/Components/_nav.scss";
+import { toggleTheme } from "../features/darkThemeSlice/themeSlice";
 
 const Nav = () => {
   const dispatch = useDispatch();
+  const darkMode = useSelector((state) => state.theme.darkMode);
 
   const handleMessageClick = () => {
     dispatch(openContactModal());
@@ -13,6 +15,13 @@ const Nav = () => {
   return (
     <div className="nav">
       <p className="nav__title">OXYgram</p>
+      <button 
+      onClick={() => dispatch(toggleTheme())}
+      className="theme-toggle-button"
+      >
+        {darkMode ? "Modo claro" : "Modo oscuro"}
+      </button>
+
       <img
         className="nav__icon_message"
         src={iconMessage}
