931fc2400ba4b25f95e3b7929ca7c38f8b9d34c6
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2025-01-13 20:05:13 +0100

    change slideBar to dark theme

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -1,19 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { openContactModal } from "../features/modals/modalSlice";
 import iconMessage from "../assets/icon_message.png";
 import icon_search_black from "../assets/icon_search_black.png";
 import icon_user from "../assets/icon_user.png";
 import icon_home from "../assets/icon_home.png";
 import "../styles/Components/_slideBar.scss";
+import { toggleTheme } from "../features/darkThemeSlice/themeSlice";
 
 const Slidebar = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const darkMode = useSelector((state) => state.theme.darkMode);
 
   const handleMessageClick = () => {
     dispatch(openContactModal());
   };
+
   return (
     <div className="slideBar">
       <h1 className="slideBar__title">OXYgram</h1>
@@ -23,18 +26,19 @@ const Slidebar = () => {
         className={"slideBar__link"}
       >
         <img className="slideBar__link__icon" src={icon_home} alt="Home" />
-        <p className={`${
-          location.pathname === "/" ? "active" : ""
-        }`}>For you</p>
+        <p className={`${location.pathname === "/" ? "active" : ""}`}>
+          For you
+        </p>
       </Link>
-      <Link
-        to="/search"
-        className={"slideBar__link"}
-      >
-        <img className="slideBar__link__icon" src={icon_search_black} alt="Search" />
-        <p className={`${
-          location.pathname === "/search" ? "active" : ""
-        }`}>Search</p>
+      <Link to="/search" className={"slideBar__link"}>
+        <img
+          className="slideBar__link__icon"
+          src={icon_search_black}
+          alt="Search"
+        />
+        <p className={`${location.pathname === "/search" ? "active" : ""}`}>
+          Search
+        </p>
       </Link>
       <Link
         to="/profile"
@@ -42,10 +46,15 @@ const Slidebar = () => {
           location.pathname === "/profile" ? "active" : ""
         }`}
       >
-        <img className="slideBar__link__icon" style={{ padding: `0em 0.4em`}} src={icon_user} alt="Profile" />
-        <p className={`${
-          location.pathname === "/profile" ? "active" : ""
-        }`}>Profile</p>
+        <img
+          className="slideBar__link__icon"
+          style={{ padding: `0em 0.4em` }}
+          src={icon_user}
+          alt="Profile"
+        />
+        <p className={`${location.pathname === "/profile" ? "active" : ""}`}>
+          Profile
+        </p>
       </Link>
       <div className="slideBar__link">
         <img
@@ -56,6 +65,12 @@ const Slidebar = () => {
         />
         <p>Message</p>
       </div>
+      <button
+        onClick={() => dispatch(toggleTheme())}
+        className="theme-toggle-button"
+      >
+        {darkMode ? "Modo Claro" : "Modo Oscuro"}
+      </button>
     </div>
   );
 };
